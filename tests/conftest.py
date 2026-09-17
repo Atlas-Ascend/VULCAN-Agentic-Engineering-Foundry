725f77aa6ea46d5f74359ec20851473319a00a51
Atlas-Ascend <ghostatlasempire@gmail.com>
2026-09-16 20:03:38 -0700

    test: execute coroutine tests with stdlib asyncio

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import asyncio
+import inspect
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "asyncio: execute coroutine test with stdlib asyncio")
+
+
+def pytest_pyfunc_call(pyfuncitem):
+    if not inspect.iscoroutinefunction(pyfuncitem.obj):
+        return None
+    kwargs = {name: pyfuncitem.funcargs[name] for name in pyfuncitem._fixtureinfo.argnames if name in pyfuncitem.funcargs}
+    asyncio.run(pyfuncitem.obj(**kwargs))
+    return True
